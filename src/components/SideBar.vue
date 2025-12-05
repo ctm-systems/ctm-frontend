77b05f51d991a10423ae42407c5179ccb8b86bf5
Barr0ca <iandearaujogalvao@gmail.com>
2025-12-05 20:06:10 -0300

    refactor&fix:  Retirando algumas linhas de código sem alteração da aplicação; Corrigindo movimento do icone de usuário

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -10,21 +10,13 @@
     <div class="drawer-content" >
 
       <!-- TOPO (avatar + nome) -->
-      <div class="user m">
-        <!-- Perfil do usuário com sidebar expandida -->
-        <div class="d-flex flex-row align-center" v-if="expanded">
-          <v-avatar size="40" class="ml-4">
-            <img src="/assets/avatar-teste.jpeg" alt="user" />
-          </v-avatar>
-          <div size="40" class="nome-usuario ml-6 mr-6">Nome do usuário</div>
-        </div>
-        <!-- Perfil do usuário com sidebar reduzida -->
-        <div v-else class="d-flex justify-center">
-          <v-avatar size="40">
-            <img src="/assets/avatar-teste.jpeg" alt="user" />
-          </v-avatar>
-        </div>
+      <div class="avatar-container">
+        <v-avatar>
+          <img src="@/../assets/avatar-teste.jpeg" alt="user" />
+        </v-avatar>
+        <div v-if="expanded" class="nome-usuario">Nome do usuário</div>
       </div>
+
       <!-- MENU PRINCIPAL -->
       <v-list nav density="comfortable">
         <v-list-item
@@ -33,10 +25,7 @@
           :prepend-icon="item.icon"
           :title="expanded ? item.title : ''"
           @click="setActive(item)"
-          :class="[
-            'menu-item',
-            item.active ? 'menu-active' : ''
-          ]" 
+          :class="['menu-item', item.active ? 'menu-active' : '']" 
           class="rounded-lg ml-2 mr-"
         ></v-list-item>
       </v-list>
@@ -57,28 +46,28 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue'
 
-const expanded = ref(false);
+const expanded = ref(false)
 
 const menu = ref([
-  { title: "Dashboard", icon: "mdi-home", active: true },
-  { title: "Lista de clientes", icon: "mdi-view-list", active: false },
-  { title: "Cadastrar cliente", icon: "mdi-account-plus", active: false },
-  { title: "Cadastrar amostra", icon: "mdi-gold", active: false },
-  { title: "Gerar orçamento", icon: "mdi-file-pdf-box", active: false },
-  { title: "Tratar excel", icon: "mdi-table", active: false },
-  { title: "Gerar laudo", icon: "mdi-file-document-edit", active: false },
-]);
+  { title: 'Dashboard', icon: 'mdi-home', active: true },
+  { title: 'Lista de clientes', icon: 'mdi-view-list', active: false },
+  { title: 'Cadastrar cliente', icon: 'mdi-account-plus', active: false },
+  { title: 'Cadastrar amostra', icon: 'mdi-gold', active: false },
+  { title: 'Gerar orçamento', icon: 'mdi-file-pdf-box', active: false },
+  { title: 'Tratar excel', icon: 'mdi-table', active: false },
+  { title: 'Gerar laudo', icon: 'mdi-file-document-edit', active: false },
+])
 
 function setActive(item) {
-  menu.value.forEach((i) => (i.active = false));
-  item.active = true;
+  menu.value.forEach((i) => (i.active = false))
+  item.active = true
 }
 
 /* Função do botão sair */
 function logout() {
-  console.log("Saindo...");
+  console.log('Saindo...')
   // Aqui você pode:
   // - limpar token
   // - redirecionar
@@ -94,7 +83,7 @@ function logout() {
 
 /* Item ativo */
 .menu-active {
-  color: #0E544A !important;
+  color: #19917f !important;
 }
 
 /* Botão de sair */
@@ -117,7 +106,6 @@ function logout() {
 .v-avatar img {
   width: 100%;
   height: 100%;
-  object-fit: cover;
 }
 
 .drawer-content {
@@ -126,9 +114,26 @@ function logout() {
   height: 100%;
 }
 
-.nome-usuario{
+.nome-usuario {
+  display: flex;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+  max-width: 200px;
+}
+
+.avatar-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 21px;
+  padding: 0 16px;
+}
+
+.v-list-item-title,
+.nome-usuario {
+  font-size: 15px !important;
+  font-family: inherit !important;
+  font-weight: 400 !important;
 }
 </style>
