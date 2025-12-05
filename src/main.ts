8c5ce2ce931b5420eea19dc92982ed902e62dbbc
Barr0ca <iandearaujogalvao@gmail.com>
2025-12-05 19:12:41 -0300

    style: Formatação

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,10 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 
-
 const vuetify = createVuetify({
-    components,
-    directives,
-  })
+  components,
+  directives,
+})
 
 const app = createApp(App)
 
