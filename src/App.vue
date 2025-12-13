d0010ba60928e6db2e531c3ce99f02d4fd4e0012
Jardson Alan <jardsonalan132@gmail.com>
2025-12-12 23:16:43 -0300

    fix: Corrigindo tela de adicionar clientes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import LoginView from './views/LoginView.vue';
-// import SideBarComponent from './components/SideBarComponent.vue';
+// import LoginView from './views/LoginView.vue';
+import SideBarComponent from './components/SideBarComponent.vue';
+import AddClientView from './views/AddClientView.vue';
 </script>
 
 <template>
   <v-responsive>
     <v-app>
-      <!-- <SideBarComponent /> -->
-      <LoginView />
+      <SideBarComponent />
+      <!-- <LoginView /> -->
+      <v-main>
+        <AddClientView />
+      </v-main>
     </v-app>
   </v-responsive>
 </template>
--- src/views/AddClientView.vue
+++ src/views/AddClientView.vue
@@ -1,94 +1,98 @@
 <template>
-  <v-container class="py-10" style="max-width: 1166px">
-    <v-row dense>
-      <!-- Nome -->
-      <v-col cols="12" md="6">
-        <TextInputComponent
-          v-model="form.nome"
-          placeholder-props="Nome do cliente"
-          prepend-icon-props="mdi-account"
-        />
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <span class="text-h6 font-weight-bold">Adicionar cliente</span>
       </v-col>
+      <v-col cols="12">
+        <v-form @submit.prevent>
+          <v-row dense>
+            <v-col cols="12" md="6">
+              <TextInputComponent
+                v-model="form.nome"
+                placeholder-props="Nome do cliente"
+                prepend-icon-props="mdi-account"
+                density="compact"
+              />
+            </v-col>
 
-      <!-- Email -->
-      <v-col cols="12" md="6">
-        <TextInputComponent
-          v-model="form.email"
-          placeholder-props="Email"
-          type-props="email"
-          prepend-icon-props="mdi-email"
-        />
-      </v-col>
+            <v-col cols="12" md="6">
+              <TextInputComponent
+                v-model="form.email"
+                placeholder-props="Email"
+                type-props="email"
+                prepend-icon-props="mdi-email"
+                density="compact"
+              />
+            </v-col>
 
-      <!-- CPF/CNPJ -->
-      <v-col cols="12" md="6">
-        <TextInputComponent
-          v-model="form.cpf"
-          placeholder-props="CPF/CNPJ"
-          prepend-icon-props="mdi-card-account-details"
-        />
-      </v-col>
+            <v-col cols="12" md="6">
+              <TextInputComponent
+                v-model="form.cpf"
+                placeholder-props="CPF/CNPJ"
+                prepend-icon-props="mdi-card-account-details"
+                density="compact"
+              />
+            </v-col>
 
-      <!-- Telefone -->
-      <v-col cols="12" md="6">
-        <TextInputComponent
-          v-model="form.telefone"
-          placeholder-props="Telefone"
-          prepend-icon-props="mdi-phone"
-        />
-      </v-col>
+            <v-col cols="12" md="6">
+              <TextInputComponent
+                v-model="form.telefone"
+                placeholder-props="Telefone"
+                prepend-icon-props="mdi-phone"
+                density="compact"
+              />
+            </v-col>
 
-      <!-- CEP -->
-      <v-col cols="12" md="4">
-        <TextInputComponent
-          v-model="form.cep"
-          placeholder-props="CEP"
-          prepend-icon-props="mdi-map-marker"
-        />
-      </v-col>
+            <v-col cols="12" md="4">
+              <TextInputComponent
+                v-model="form.cep"
+                placeholder-props="CEP"
+                prepend-icon-props="mdi-map-marker"
+                density="compact"
+              />
+            </v-col>
 
-      <!-- Endereço -->
-      <v-col cols="12" md="8">
-        <TextInputComponent
-          v-model="form.endereco"
-          label="Endereço"
-          placeholder-props="Endereço"
-          prepend-icon-props="mdi-home"
-        />
-      </v-col>
+            <v-col cols="12" md="8">
+              <TextInputComponent
+                v-model="form.endereco"
+                label="Endereço"
+                placeholder-props="Endereço"
+                prepend-icon-props="mdi-home"
+                density="compact"
+              />
+            </v-col>
 
-      <!-- Técnicos -->
-      <v-col cols="12" md="8">
-        <v-select
-          v-model="form.tecnicos"
-          :items="tecnicosLista"
-          label="Técnico(s) responsável(is)"
-          multiple
-          chips
-          variant="outlined"
-          class="mt-5 rounded-lg"
-        />
-      </v-col>
+            <v-col cols="12" md="8">
+              <v-select
+                v-model="form.tecnicos"
+                :items="tecnicosLista"
+                label="Técnico(s) responsável(is)"
+                multiple
+                chips
+                variant="outlined"
+                class="rounded-lg"
+                density="compact"
+              />
+            </v-col>
 
-      <!-- Data -->
-      <v-col cols="12" md="4">
-        <v-text-field
-          v-model="form.data"
-          type="date"
-          label="Data de recebimento"
-          variant="outlined"
-          class="mt-5 rounded-lg"
-        />
-      </v-col>
-    </v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="form.data"
+                type="date"
+                label="Data de recebimento"
+                variant="outlined"
+                class="rounded-lg"
+                density="compact"
+              />
+            </v-col>
+          </v-row>
 
-    <!-- Botões -->
-    <v-row justify="end" class="mt-10">
-      <v-col cols="auto">
-        <v-btn color="#FF1A1A"> Limpar campos </v-btn>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn color="#00A400"> Adicionar </v-btn>
+          <div class="d-flex flex-column flex-md-row ga-3 justify-md-end">
+            <v-btn color="#FF1A1A">Limpar campos</v-btn>
+            <v-btn color="#00A400">Adicionar</v-btn>
+          </div>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
