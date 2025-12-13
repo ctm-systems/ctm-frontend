f07195e3e106f3826774925eaf4f9c41b9694cd8
Jardson Alan <jardsonalan132@gmail.com>
2025-12-12 23:04:00 -0300

    fix: Corrigindo tela de cadastro de amostras

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import LoginView from './views/LoginView.vue';
-// import SideBarComponent from './components/SideBarComponent.vue';
+// import LoginView from './views/LoginView.vue';
+import AddSampleView from './views/AddSampleView.vue';
 </script>
 
 <template>
   <v-responsive>
     <v-app>
-      <!-- <SideBarComponent /> -->
-      <LoginView />
+      <SideBarComponent />
+      <!-- <LoginView /> -->
+      <v-main>
+        <AddSampleView />
+      </v-main>
     </v-app>
   </v-responsive>
 </template>
--- src/views/AddSampleView.vue
+++ src/views/AddSampleView.vue
@@ -1,132 +1,55 @@
+<script setup lang="ts">
+import TextInputComponent from '@/components/TextInputComponent.vue'
+</script>
+
 <template>
-  <v-container class="py-10" style="max-width: 1166px">
-
+  <v-container fluid>
     <v-row>
-        <v-col cols="6" md="6">
-            <!-- Preview da imagem -->
-            <v-img
-                v-if="imagePreview"
-                :src="imagePreview"
-                max-height="317"
-                class="rounded-lg"
-                cover
-            />
-            <!-- Foto da Amostra -->
-            <v-file-input
-                v-model="form.foto"
-                label="Foto da Amostra"
-                prepend-icon="mdi-camera"
-                class="mt-5 rounded-lg"
-                accept="image/*"
-                @update:model-value="onFileChange"
-            />
-        </v-col>
-    </v-row>
-
-    <v-row dense>
-        <!-- Identificação da Amostra -->
-        <v-col cols="12" md="6">
-            <TextInputComponent
-            v-model="form.identificacao"
-            placeholder-props="Identificação da Amostra"
-            prepend-icon-props="mdi-email"
-            />
-        </v-col>
-
-        <!-- Tipo da Amostra -->
-        <v-col cols="12" md="6">
-            <v-select
-            v-model="form.tipo"
-            :items="tipoAmostra"
-            label="Tipo da Amostra"
-            multiple
-            chips
-            variant="outlined"
-            class="mt-5 rounded-lg"
-            />
-        </v-col>
-
-      <!-- Cliente -->
-        <v-col cols="12" md="6">
-            <TextInputComponent
-            v-model="form.cliente"
-            placeholder-props="Cliente"
-            prepend-icon-props="mdi-account"
-        />
-        </v-col>
-      
-        <!-- Processos -->
-        <v-col cols="12" md="6">
-          <TextInputComponent
-            v-model="form.processos"
-            placeholder-props="Processos"
-            prepend-icon-props="mdi-file-document"
-          />
-        </v-col>
-
-    </v-row>
-
-    <!-- Botões -->
-    <v-row justify="end" class="mt-10">
-      <v-col cols="auto">
-        <v-btn color="#FF1A1A" @click="limparCampos"> Limpar campos </v-btn>
+      <v-col cols="12">
+        <span class="text-h6 font-weight-bold">Cadastrar amostra</span>
       </v-col>
-      <v-col cols="auto">
-        <v-btn color="#00A400"> Adicionar </v-btn>
+      <v-col cols="12">
+        <v-form>
+          <v-file-input
+            label="Foto da amostra"
+            prepend-icon="mdi-camera"
+            variant="outlined"
+            density="compact"
+          />
+
+          <v-select
+            label="Cliente"
+            :items="['Cliente 1', 'Cliente 2', 'Cliente 3']"
+            variant="outlined"
+            density="compact"
+          />
+
+          <TextInputComponent
+            label="Identificação da amostra"
+            variant="outlined"
+            density="compact"
+          />
+
+          <v-select
+            label="Tipo de amostra"
+            :items="['Rocha', 'Areia']"
+            variant="outlined"
+            density="compact"
+          />
+
+          <v-select
+            label="Processos"
+            :items="['FRX', 'DRX', 'Moagem']"
+            variant="outlined"
+            density="compact"
+          />
+
+          <div class="d-flex flex-column flex-md-row ga-3 justify-md-end">
+            <v-btn color="#FF1A1A">Limpar campos</v-btn>
+            <v-btn color="#00A400">Adicionar</v-btn>
+          </div>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import TextInputComponent from '@/components/TextInputComponent.vue'
-
-// Dados do formulário
-interface SampleForm {
-    foto: File | null
-    cliente: string
-    identificacao: string
-    tipo: string[]
-    processos: string
-}
-const form = ref<SampleForm>({
-    foto: null,
-    cliente: '',
-    identificacao: '',
-    tipo: [],
-    processos: '',
-})
-
-// Lista de tipos de amostra
-const tipoAmostra = ['Rocha', 'Areia']
-
-// Preview da imagem
-const imagePreview = ref<string | null>(null)
-
-// Quando o arquivo mudar
-function onFileChange(file: File | File[] | null) {
-  if (!file) {
-    form.value.foto = null
-    imagePreview.value = null
-    return
-  }
-  const selectedFile = Array.isArray(file) ? file[0] : file
-  if (selectedFile instanceof File){
-    form.value.foto = selectedFile
-    imagePreview.value = URL.createObjectURL(selectedFile)  
-  }
-}
-
-function limparCampos() {
-    form.value = {
-        foto: null,
-        cliente: '',
-        identificacao: '',
-        tipo: [],
-        processos: '',
-    }
-    imagePreview.value = null
-}
-
-</script>
