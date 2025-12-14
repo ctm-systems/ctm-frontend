3a0f26789b50716e669e9f8f95bf7880893997e2
Lucas Dantas <162215028+uluscaz-ifrn@users.noreply.github.com>
2025-12-13 21:50:11 -0300

    feat: tela de gerar orçamento

---
+++ src/views/BudgetView.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import TextInputComponent from '@/components/TextInputComponent.vue'
+</script>
+
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <span class="text-h6 font-weight-bold">Gerar Orçamento</span>
+      </v-col>
+      <v-col cols="12">
+
+        <v-form @submit.prevent>
+            <TextInputComponent
+                placeholder-props="Identificação do orçamento"
+                density="compact"
+              />
+
+          <v-select
+            label="Cliente"
+            :items="['Cliente 1', 'Cliente 2', 'Cliente 3']"
+            variant="outlined"
+            density="compact"
+          />
+
+            <v-select
+            label="Amostra"
+            variant="outlined"
+            density="compact"
+            :items="['Amostra 1', 'Amostra 2', 'Amostra 3']"
+          />
+          
+            <TextInputComponent
+                placeholder-props="Valor do orçamento"
+                density="compact"
+              />
+
+          <div class="d-flex flex-column flex-md-row ga-3 justify-md-end">
+            <v-btn color="#FF1A1A">Limpar campos</v-btn>
+            <v-btn color="#00A400">Gerar Orçamento</v-btn>
+          </div>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped></style>
