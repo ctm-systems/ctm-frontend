111dbea0b744524fda9059c2fa1ee58382bba601
Jardson Alan <jardsonalan132@gmail.com>
2026-01-23 22:20:02 -0300

    chore: Atualiza API URL para usar variável de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
+import { API_URL } from "@/config/env"
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: API_URL,
   withCredentials: true,
 })
