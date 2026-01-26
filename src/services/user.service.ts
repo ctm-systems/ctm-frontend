dadb17de61c19a2405d126eb00dfdfcb9ea4d275
Jardson Alan <jardsonalan132@gmail.com>
2026-01-26 18:24:01 -0300

    refactor: Remove arquivos desnecessários

--- src/services/user.service.ts
+++
@@ -1,7 +0,0 @@
-import { api } from './api'
-import type { User } from '@/types/User'
-
-export async function getUsers(): Promise<User[]> {
-  const { data } = await api.get<User[]>('/users')
-  return data
-}
--- src/types/User.ts
+++
@@ -1,5 +0,0 @@
-export interface User {
-  id: number
-  matricula: string
-  nome: string
-}
