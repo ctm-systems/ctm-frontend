6e69a6fff21f43e4cad0a96a2df360ccd6419446
Barr0ca <iandearaujogalvao@gmail.com>
2026-01-28 13:49:39 -0300

    feat: Adicionando propriedade de mascara para componente de input

--- src/components/TextInputComponent.vue
+++ src/components/TextInputComponent.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { vMaska } from 'maska/vue'
+
 defineProps({
   placeholderProps: String,
   typeProps: { type: String, default: 'text' },
   prependIconProps: String,
   appendIconProps: String,
+  maskProps: String,
 })
 
 </script>
@@ -14,6 +17,7 @@ defineProps({
     :type="typeProps"
     :prepend-inner-icon="prependIconProps"
     :append-inner-icon="appendIconProps"
+    v-maska="maskProps"
     variant="outlined"
     class="rounded-lg"
   />
